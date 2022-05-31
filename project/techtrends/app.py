458481a0d50a398f79b899a74c4b6a2bb2f37813
Rohithmarktricks <rohithbanka05@gmail.com>
2022-05-31 19:24:40 +0530

    Docker image

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+import logging
+from time import asctime
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
@@ -18,22 +19,22 @@ def get_post(post_id):
     connection.close()
     return post
 
-def check_db_connection():
-    """Checks if connecting to database is successful"""
-    try:
-        connection = get_db_connection()
-        connection.close()
-    except:
-        raise Exception("Database connection failure")
+# def check_db_connection():
+#     """Checks if connecting to database is successful"""
+#     try:
+#         connection = get_db_connection()
+#         connection.close()
+#     except:
+#         raise Exception("Database connection failure")
     
-def post_table_exists():
-    """Checks if the post table exists"""
-    try:
-        connection = get_db_connection()
-        connection.execute("SELECT 1 FROM posts").fetchone()
-        connection.close()
-    except:
-        raise Exception("Table "posts" does not exist")
+# def post_table_exists():
+#     """Checks if the post table exists"""
+#     try:
+#         connection = get_db_connection()
+#         connection.execute("SELECT 1 FROM posts").fetchone()
+#         connection.close()
+#     except:
+#         raise Exception("Table "posts" does not exist")
 
 
 
@@ -41,12 +42,27 @@ def post_table_exists():
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
+
+def log_info_level(message):
+    """
+    Add info level log to the app's logger
+    ======================================
+
+    Parameters
+    ----------
+    message : str
+        Message to log.
+    """
+
+    app.logger.info('{}:{}'.format(asctime(), message))
+
 # Define the main route of the web application 
 @app.route('/')
 def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
+    log_info_level('2020 CNCF Annual Report is retrieved.')
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered 
@@ -55,13 +71,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        log_info_level('Post: Failed to get #{}.'.format(post_id))
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        log_info_level('Post: Article "{}" is retrieved'.format(post['title']))
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    log_info_level('About Us page is retrieved.')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -79,26 +98,52 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            log_info_level('Create: new article created "{}".'.format(title))
             return redirect(url_for('index'))
 
+    log_info_level('Create: Failed to create article because of missing title.')
     return render_template('create.html')
 
-@app.route('/healthz', methods=['GET'])
+# Define the Healthcheck endpoint
+@app.route('/healthz')
 def healthz():
+    """
+    Added function that peforms health check
+    ========================================
+    """
 
-    response_body = {'result': 'OK - healthy'}
-    status_code = 200
-
+    response = app.response_class(response=json.dumps({'result' : 'OK - healthy'}),
+                                  status=200, mimetype='application/json')
     try:
-        check_db_connection()
-        post_table_exists()
-    except Exception as e:
-        response_body['result'] = 'ERROR - unhealthy'
-        response_body['details'] = str(e)
+        connection = get_db_connection()
+        connection.execute('SELECT 1 FROM posts').fetchone()
+        connection.close()
+        app.logger.info('Healthcheck: ok')
+    except Exception:
+        response = app.response_class(
+                        response=json.dumps({'result' : 'Error - not healthy'}),
+                                      status=200, mimetype='application/json')
+        log_info_level('Healthcheck: failed')
+    return response
+
+@app.route('/metrics')
+def metrics():
+
+    global connections_count
+
+    connection = get_db_connection()
+    helper = connection.execute('SELECT COUNT(*) FROM posts').fetchone()
+    posts_count = helper[0]
+    connection.close()
+    log_info_level('Metrics: @ {} posts, {} connections'.format(posts_count, connections_count))
+
+    return app.response_class(response=json.dumps(
+        {'db_connection_count': connections_count, 'post_count': posts_count}),
+    status=200, mimetype='application/json')
         
 
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(level=logging.DEBUG)
+    app.run(host='0.0.0.0', port='3111')
